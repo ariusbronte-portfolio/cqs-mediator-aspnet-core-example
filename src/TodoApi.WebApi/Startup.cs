104ead3dd07666a5cceab29ed6469b986115b166
Arius Bronte <ariusbronte@gmail.com>
2020-03-03 20:27:16 +0000

    cleanup code: added XML comments in Startup.cs

--- src/TodoApi.WebApi/Startup.cs
+++ src/TodoApi.WebApi/Startup.cs
@@ -1,32 +1,49 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace TodoApi.WebApi
 {
+    /// <summary>
+    ///      Configures services and the app's request pipeline.
+    /// </summary>
     public class Startup
     {
+        /// <inheritdoc cref="Microsoft.Extensions.Configuration.IConfiguration"/>
         private readonly IConfiguration _configuration;
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="TodoApi.WebApi.Startup"/> class.
+        /// </summary>
+        /// <param name="configuration">
+        ///     Represents a set of key/value application configuration properties.
+        /// </param>
         public Startup(IConfiguration configuration)
         {
             _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
         }
 
+        /// <summary>
+        ///     This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services">Defines a contract for a collection of service descriptors.</param>
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
         }
 
+        /// <summary>
+        ///     This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app">
+        ///     Provides the mechanisms to configure an application's request pipeline.
+        /// </param>
+        /// <param name="env">
+        ///     Provides information about the web hosting environment an application is running in.
+        /// </param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -35,11 +52,8 @@ namespace TodoApi.WebApi
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
