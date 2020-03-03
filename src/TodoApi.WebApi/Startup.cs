027e4fa1640417387da4a0183f3ac645d7dbe070
Arius Bronte <ariusbronte@gmail.com>
2020-03-03 20:19:34 +0000

    cleanup code: remove initial comments and change initial structure

--- src/TodoApi.WebApi/Startup.cs
+++ src/TodoApi.WebApi/Startup.cs
@@ -15,20 +15,18 @@ namespace TodoApi.WebApi
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
