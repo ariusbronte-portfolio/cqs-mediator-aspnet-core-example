d41924a53a51f8752f90a5a388b9d05883be02e6
Arius Bronte <ariusbronte@gmail.com>
2020-04-02 23:06:37 +0000

    initialization of MinDataRate in a separate method has been made

--- src/TodoApi.WebApi/Extensions/WebHostBuilderExtensions.cs
+++ src/TodoApi.WebApi/Extensions/WebHostBuilderExtensions.cs
@@ -34,15 +34,30 @@ namespace TodoApi.WebApi.Extensions
                 serverOptions.Limits.MaxConcurrentConnections = 100;
                 serverOptions.Limits.MaxConcurrentUpgradedConnections = 100;
                 serverOptions.Limits.MaxRequestBodySize = 10 * 1024;
-                serverOptions.Limits.MinRequestBodyDataRate =
-                    new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(value: 10));
-                serverOptions.Limits.MinResponseDataRate =
-                    new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(value: 10));
+                serverOptions.Limits.MinRequestBodyDataRate = 
+                    CreateMinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(value: 10));
+                serverOptions.Limits.MinResponseDataRate = 
+                    CreateMinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(value: 10));
                 serverOptions.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(value: 2);
                 serverOptions.Limits.RequestHeadersTimeout = TimeSpan.FromMinutes(value: 1);
             });
 
             return hostBuilder;
+        }
+        
+        /// <summary>
+        ///     Creates a new instance of <see cref="Microsoft.AspNetCore.Server.Kestrel.Core.MinDataRate"/>.
+        /// </summary>
+        /// <param name="bytesPerSecond">
+        ///     The minimum rate in bytes/second at which data should be processed.
+        /// </param>
+        /// <param name="gracePeriod">
+        ///     The amount of time to delay enforcement of <paramref name="bytesPerSecond"/>,
+        ///     starting at the time data is first read or written.
+        /// </param>
+        private static MinDataRate CreateMinDataRate(double bytesPerSecond, TimeSpan gracePeriod)
+        {
+            return new MinDataRate(bytesPerSecond: bytesPerSecond, gracePeriod: gracePeriod);
         }
     }
 }
