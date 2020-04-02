424603b697987c8e4c33228b6c3fcbfb72d8b0d7
Arius Bronte <ariusbronte@gmail.com>
2020-04-02 22:53:06 +0000

    added GetTodoItemQuery and GetTodoItemQueryHandler in TodoApi.BusinessLogic

---
+++ src/TodoApi.BusinessLogic/TodoItem/Queries/GetTodoItem/GetTodoItemQuery.cs
@@ -0,0 +1,21 @@
+using MediatR;
+using TodoApi.Abstractions.Dto;
+using TodoApi.Abstractions.Dto.TodoItemDtos;
+
+namespace TodoApi.BusinessLogic.TodoItem.Queries.GetTodoItem
+{
+    public class GetTodoItemQuery : IRequest<TodoItemDto>
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="TodoApi.BusinessLogic.TodoItem.Queries.GetTodoItem.GetTodoItemQuery"/> class.
+        /// </summary>
+        /// <param name="id">The primary key for this task.</param>
+        public GetTodoItemQuery(long id)
+        {
+            Id = id;
+        }
+
+        /// <inheritdoc cref="TodoApi.Domain.Entities.TodoItemEntity.Id"/>
+        public long Id { get; }
+    }
+}
---
+++ src/TodoApi.BusinessLogic/TodoItem/Queries/GetTodoItem/GetTodoItemQueryHandler.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Abstractions.Dto;
+using TodoApi.Abstractions.Dto.TodoItemDtos;
+using TodoApi.DataAccess;
+using TodoApi.Domain.Entities;
+using TodoApi.Implementations.Exceptions;
+
+namespace TodoApi.BusinessLogic.TodoItem.Queries.GetTodoItem
+{
+    public class GetTodoItemQueryHandler : IRequestHandler<GetTodoItemQuery, TodoItemDto>
+    {
+        private readonly TodoApiDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetTodoItemQueryHandler(TodoApiDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(paramName: nameof(dbContext));
+            _mapper = mapper ?? throw new ArgumentNullException(paramName: nameof(mapper));
+        }
+
+        public async Task<TodoItemDto> Handle(
+            GetTodoItemQuery request,
+            CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            var id = request.Id;
+            
+            var entity = await _dbContext.TodoItems.FindAsync(id);
+
+            if (entity == null)
+            {
+                throw new RecordNotFoundException(entity: nameof(TodoItemEntity), key: id);
+            }
+
+            return _mapper.Map<TodoItemDto>(source: entity);
+        }
+    }
+}
