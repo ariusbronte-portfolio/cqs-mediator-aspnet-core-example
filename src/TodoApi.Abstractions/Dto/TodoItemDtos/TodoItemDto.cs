919a3409ae90077aa5fe19448ee906a10b54bdb3
Arius Bronte <ariusbronte@gmail.com>
2020-04-02 22:45:04 +0000

    create TodoItemDto.cs

---
+++ src/TodoApi.Abstractions/Dto/TodoItemDtos/TodoItemDto.cs
@@ -0,0 +1,30 @@
+﻿using TodoApi.Abstractions.Enums;
+
+namespace TodoApi.Abstractions.Dto.TodoItemDtos
+{
+    /// <summary>
+    ///     Presents the essence of the tasks.
+    /// </summary>
+    public class TodoItemDto
+    {
+        /// <summary>
+        ///     The primary key for this task.
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        ///     Task title.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        ///     Normalized task title.
+        /// </summary>
+        public string NormalizedTitle { get; set; }
+
+        /// <summary>
+        ///     Task status.
+        /// </summary>
+        public TodoTaskStatus Status { get; set; }
+    }
+}
