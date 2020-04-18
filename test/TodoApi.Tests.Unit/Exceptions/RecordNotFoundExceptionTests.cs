71c0add00e4e06d66cd23d8b424393cbc0002b26
Arius Bronte <ariusbronte@gmail.com>
2020-04-18 22:27:56 +0000

    Create RecordNotFoundExceptionTests.cs

---
+++ test/TodoApi.Tests.Unit/Exceptions/RecordNotFoundExceptionTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using TodoApi.Implementations.Exceptions;
+using Xunit;
+
+namespace TodoApi.Tests.Unit.Exceptions
+{
+    public class RecordNotFoundExceptionTests
+    {
+        [Fact]
+        public void WithCustomErrorMessage()
+        {
+            const string entity = "entity";
+            const long key = 1;
+            
+            // Arrange
+            var exception = new RecordNotFoundException(entity, key);
+            
+            // Assert
+            exception.Message.Should().Be($"Entity '{entity}' does not matter with the '{key}' key.");
+        }
+    }
+}
